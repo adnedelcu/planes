ed158afd6c3c720e049f1371a2f1351cbd3571b3
Alexandru-Daniel Nedelcu <alexnedelcu0@gmail.com>
2025-07-05 15:58:23 +0200

    Added colors

--- main.cpp
+++ main.cpp
@@ -2,7 +2,31 @@
 
 using namespace std;
 
-string WATER = "▓";
+#define RESET "\033[0m"
+#define BLACK "\033[30m"              /* Black */
+#define RED "\033[31m"                /* Red */
+#define GREEN "\033[32m"              /* Green */
+#define YELLOW "\033[33m"             /* Yellow */
+#define BLUE "\033[34m"               /* Blue */
+#define MAGENTA "\033[35m"            /* Magenta */
+#define CYAN "\033[36m"               /* Cyan */
+#define WHITE "\033[37m"              /* White */
+#define BG_BLACK "\033[40m"            /* Backgroun Black */
+#define BG_RED "\033[41m"              /* Backgroun Red */
+#define BG_GREEN "\033[42m"            /* Backgroun Green */
+#define BG_YELLOW "\033[43m"           /* Backgroun Yellow */
+#define BG_BLUE "\033[44m"             /* Backgroun Blue */
+#define BG_MAGENTA "\033[45m"          /* Backgroun Magenta */
+#define BG_CYAN "\033[46m"             /* Backgroun Cyan */
+#define BG_WHITE "\033[47m"            /* Backgroun White */
+#define BOLD_BLACK "\033[1m\033[30m"   /* Bold Black */
+#define BOLD_RED "\033[1m\033[31m"     /* Bold Red */
+#define BOLD_GREEN "\033[1m\033[32m"   /* Bold Green */
+#define BOLD_YELLOW "\033[1m\033[33m"  /* Bold Yellow */
+#define BOLD_BLUE "\033[1m\033[34m"    /* Bold Blue */
+#define BOLD_MAGENTA "\033[1m\033[35m" /* Bold Magenta */
+#define BOLD_CYAN "\033[1m\033[36m"    /* Bold Cyan */
+#define BOLD_WHITE "\033[1m\033[37m"   /* Bold White */
 
 /*
  ┤ ╡ ╖ ╕ ╣ ║ ╜
@@ -26,6 +50,9 @@ South        West
   ╨          ║ └
              ╙
 */
+
+/** Water component */
+string WATER = "▓";
 
 /** North orientation plane components */
 string NOSE_N = "╥";
@@ -228,7 +255,12 @@ void show_terrain(const string(* p1)[10]) {
                 cout << j;
                 continue;
             }
+            cout << BLUE;
+            if (p1[i - 1][j - 1] != WATER) {
+                cout << BG_BLUE << WHITE;
+            }
             cout << p1[i - 1][j - 1];
+            cout << RESET;
         }
         cout << endl;
     }
